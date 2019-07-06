c7cffa15ba1d9f23e37199d3b207ef88660b7ba7
Sarah Harrs <sarahharrs@gmail.com>
2019-07-06 16:51:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+array  = [ 7,3,1,2,6,5]
+def sort_array_asc
+  array.sort do |a, b|
+    a <==> b
+  end
 
+def sort_array_desc
+  def sort_array_asc
+  array.sort do |a, b|
+    b<==>a
+  end
+  
+  def sort_array_char_count
+  array.sort. length do |number|
+  end
+  
+    
