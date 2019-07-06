455e968cb2274760a7739a451ad2338fdc369956
Sarah Harrs <sarahharrs@gmail.com>
2019-07-06 16:55:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,46 @@ def sort_array_desc
   end
   
   def sort_array_char_count
-  array.sort. length do |number|
+  array.sort do |a,b|
+    a.length <==> b.length
+  end
+end
+    
+    def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
   end
   
-    
